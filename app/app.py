456b9edfb547af50f953b3a9181861a74d3079cb
Home <JoaoRoccella@users.noreply.github.com>
2025-10-02 00:32:59 -0300

    feat: consulta dados por variável e por período

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from services.mqtt_handler import connect_mqtt, subscribe
+from typing import Optional
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -25,6 +26,49 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/ping")
+@app.get("/helloThere")
 def ping():
-    return {"status": "ok"}
+    return {"line": "General Kenobi!"}
+
+
+@app.get("/{variable}/last")
+@app.get("/{variable}/last/{time}/{unit}")
+def get_last_variable(variable: str, time: Optional[str] = None, unit: Optional[str]= None):
+    from models.database import Database
+
+    # mapeamento para unidades MySQL (sempre em singular)
+    units_allowed = {
+        "minutes": "MINUTE", "minute": "MINUTE", 
+        "hours": "HOUR", "hour": "HOUR", 
+        "days": "DAY", "day": "DAY"
+    }
+
+    # validação inicial
+    if time and unit:  # caso exista intervalo
+        if not (time.isdigit() and unit.lower() in units_allowed):
+            return {"erro": "Parâmetros inválidos."}
+
+
+    # Query base
+    query = "SELECT `variable`, `value`, `timestamp` FROM `data` WHERE `variable` = %s"
+
+    if time and unit:  # intervalo
+        unit_sql = units_allowed[unit.lower()]
+        query += f" AND `timestamp` >= NOW() - INTERVAL {int(time)} {unit_sql} ORDER BY `timestamp` DESC"
+    else:  # último registro
+        query += " ORDER BY `timestamp` DESC LIMIT 1"
+    
+
+    try:
+        with Database() as db:
+            result = db.executar_query(query, (variable,))
+            if result:
+                return result
+            else:
+                return {
+                    "mensagem": f"Nenhum registro de {variable} foi encontrado.",
+                    "query": query, "params": (variable,)}
+    except Exception as e:
+        return {"erro": str(e), "query": query}
+
+
